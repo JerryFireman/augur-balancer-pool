4056a3ea8951c1a477af62a7ba305c1f64424ceb
jerryfireman <jerry_fireman@strucinfo.com>
2020-08-24 17:11:54 -0400

    updated 2_deploy_factories

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -1,28 +1,13 @@
 const BFactory = artifacts.require('BFactory');
-const PoolManager = artifacts.require('PoolManager');
-const Weth = artifacts.require('Weth');
+const BPool = artifacts.require('BPool');
+const Yes = artifacts.require('Yes');
+const No = artifacts.require('No')
 const Dai = artifacts.require('Dai')
-const Mkr = artifacts.require('Mkr')
 module.exports = function(deployer) {
-  deployer.deploy(Weth, 'Wrapped Ether', 'WETH', 18);
+  deployer.deploy(Yes, 'Yes', 'YES', 18);
+  deployer.deploy(No, 'No', 'NO', 18)
   deployer.deploy(Dai, 'Dai Stablecoin', 'DAI', 18)
-  deployer.deploy(Mkr, 'Maker', 'MKR', 18)
   deployer.deploy(BFactory).then(function() {
-    return deployer.deploy(PoolManager, BFactory.address);
+    return deployer.deploy(BPool);
   });
 };
-
-
-/*
-const TMath = artifacts.require('TMath');
-const BToken = artifacts.require('BToken');
-const BFactory = artifacts.require('BFactory');
-const PoolManager = artifacts.require('PoolManager');
-
-module.exports = async function (deployer, network, accounts) {
-    if (network === 'development' || network === 'coverage') {
-        deployer.deploy(TMath);
-    }
-    deployer.deploy(PoolManager);
-};
-*/
