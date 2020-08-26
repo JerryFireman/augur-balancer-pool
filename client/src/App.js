5c1271382dde799c435ee5e5dc1a73a48d7d9da2
jerryfireman <jerry_fireman@strucinfo.com>
2020-08-26 18:00:56 -0400

    yes, no and dai tokens bound and set public swap

--- client/src/App.js
+++ client/src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
       // @ mint Dai and send to Trader1
       await daiContract.methods.mint(accounts[2], web3.utils.toWei('5000')).send({ from: accounts[0] });
 
-      // create a new balancer pool and save address to state, bind tokens, set swap fee and set public
+      // create a new balancer pool and save address to state, bind tokens and set public
       const tx = await bfactoryContract.methods.newBPool().send({from: accounts[1], gas: 6000000 });
       var bpoolAddress = tx.events.LOG_NEW_POOL.returnValues[1]
       this.setState({ bpoolAddress: bpoolAddress })
@@ -98,35 +98,56 @@ class App extends Component {
       await pool.methods.bind(yesContract.options.address, web3.utils.toWei('5000'), web3.utils.toWei('18.75')).send( {from: accounts[1], gas: 6000000 });
       await noContract.methods.approve(this.state.bpoolAddress, web3.utils.toWei('5000')).send( {from: accounts[1], gas: 6000000 });
       await pool.methods.bind(noContract.options.address, web3.utils.toWei('5000'), web3.utils.toWei('6.25')).send( {from: accounts[1], gas: 6000000 });
+      await daiContract.methods.approve(this.state.bpoolAddress, web3.utils.toWei('5000')).send( {from: accounts[1], gas: 6000000 });
+      await pool.methods.bind(daiContract.options.address, web3.utils.toWei('5000'), web3.utils.toWei('25')).send( {from: accounts[1], gas: 6000000 });
+      await pool.methods.setPublicSwap(true).send( {from: accounts[1], gas: 6000000 });
 
-
-
-
-
+      // print back end parameters to console.log
       var LP1YesBalance = await yesContract.methods.balanceOf(accounts[1]).call();
       LP1YesBalance = web3.utils.fromWei(LP1YesBalance)
       console.log("LP1 Yes balance: ", LP1YesBalance)
+
       var LP1NoBalance = await noContract.methods.balanceOf(accounts[1]).call();
       LP1NoBalance = web3.utils.fromWei(LP1NoBalance)
       console.log("LP1 No balance: ", LP1NoBalance)
+
       var LP1DaiBalance = await daiContract.methods.balanceOf(accounts[1]).call();
       LP1DaiBalance = web3.utils.fromWei(LP1DaiBalance)
       console.log("LP1 Dai balance: ", LP1DaiBalance)
+
       var Trader1DaiBalance = await daiContract.methods.balanceOf(accounts[2]).call();
       Trader1DaiBalance = web3.utils.fromWei(Trader1DaiBalance);
       console.log("Trader1 Dai balance: ", Trader1DaiBalance);
+
       var numberOfTokens = await pool.methods.getNumTokens().call();
       console.log("NumberOfTokens: ", numberOfTokens);
+
       var poolYesBalance = await pool.methods.getBalance(yesContract.options.address).call();
       poolYesBalance = web3.utils.fromWei(poolYesBalance);
       console.log("poolYesBalance: ", poolYesBalance);
+
+      var poolYesNormWeight = await pool.methods.getNormalizedWeight(yesContract.options.address).call();
+      poolYesNormWeight = web3.utils.fromWei(poolYesNormWeight);
+      console.log("poolYesNormWeight: ", poolYesNormWeight);
+
       var poolNoBalance = await pool.methods.getBalance(noContract.options.address).call();
       poolNoBalance = web3.utils.fromWei(poolNoBalance);
       console.log("poolNoBalance: ", poolNoBalance);
 
+      var poolNoNormWeight = await pool.methods.getNormalizedWeight(noContract.options.address).call();
+      poolNoNormWeight = web3.utils.fromWei(poolNoNormWeight);
+      console.log("poolNoNormWeight: ", poolNoNormWeight);
 
+      var poolDaiBalance = await pool.methods.getBalance(daiContract.options.address).call();
+      poolDaiBalance = web3.utils.fromWei(poolDaiBalance);
+      console.log("poolDaiBalance: ", poolDaiBalance);
 
+      var poolDaiNormWeight = await pool.methods.getNormalizedWeight(daiContract.options.address).call();
+      poolDaiNormWeight = web3.utils.fromWei(poolDaiNormWeight);
+      console.log("poolDaiNormWeight: ", poolDaiNormWeight);
 
+      var isPublicSwap = await pool.methods.isPublicSwap().call();
+      console.log("isPublicSwap: ", isPublicSwap)
 
 
     } catch (error) {
